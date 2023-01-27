e8c9e66b33b606247e8ff5b7b6422175910ac697
George <TheKillaCake@gmail.com>
2023-01-27 15:40:55 +0000

    Create main.py
    Testing the python-package workflow runs

---
+++ main.py
@@ -0,0 +1,35 @@
+class Words:
+    def __init__(self):
+        self.contents = list()
+        # TODO: self.contents = read in the whole file to a list
+        
+    def __repr__(self):
+        
+        # TODO: format self.contents more appropriately
+        
+        return str(self.contents)
+    
+    def ofLength(self, rule):
+        
+        try: 
+            length = int(rule)
+            self.contents = []
+            # TODO: modify self.contents to follow the rule that the length must equal exactly [num]
+            
+        except:
+            condition = str(rule)
+            operand, length = condition[0:-lengthofNum], condition[-lengthofNum]
+            # TODO: determine the length of the number and the length of the condition submitted by the user
+            # TODO: modify self.contents to follow the rule [condition] [num]
+            
+        return self
+    
+    def asPhonemes(self):
+        
+        #TODO: modify self.contents such that all words composed of letters are replaced with their phoneme counterparts
+        
+        return self
+    
+words = Words()
+words.asPhonemes().ofLength(3)
+            
